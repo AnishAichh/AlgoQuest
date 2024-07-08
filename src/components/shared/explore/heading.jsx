93275f8de4e306c62768bc5ad0bdf743e9f53476
somir02 <somirbaruah1@gmail.com>
2024-07-08 11:21:57 +0530

    explore page initialized

--- src/components/shared/explore/heading.jsx
+++ src/components/shared/explore/heading.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Heading = () =>{
+const Heading = () => {
     return (
         <>
-            <div className="text-blue">Welcome To </div>
-            <div>JQuest Explore:</div>
-            <div>Mastery Through Structured Learning Paths</div>
+            <div className="font-bold text-3xl px-24 pb-20">
+                <div className="text-indigo-800 ">Welcome To </div>
+                <div>JQuest Explore:<br/>Mastery Through Structured Learning Paths</div>
+            </div>
         </>
     )
 }

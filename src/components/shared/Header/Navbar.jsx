fbd16f0b0446a5787ebda323a270f84fe5f3c138
Rituraj Deka <dekarituraj95@gmail.com>
2024-07-12 12:15:39 +0530

    add logout button in the navbar

--- src/components/shared/Header/Navbar.jsx
+++ src/components/shared/Header/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { auth } from "../../../firebase"; 
+
 
 export default function Navbar() {
+    async function handleLogout() {
+        try {
+          await auth.signOut();
+          window.location.href = "/login";
+          console.log("User logged out successfully!");
+        } catch (error) {
+          console.error("Error logging out:", error.message);
+        }
+      }
+    
     return (
         <div className=' text-white flex justify-between items-center w-5/6 mx-auto py-6'>
             <div className='text-2xl font-bold'>
@@ -16,6 +27,7 @@ export default function Navbar() {
                 <button type='button' className='flex items-center justify-center w-10 h-10 rounded-full bg-yellow-400 hover:bg-yellow-500 transition duration-300'>
                     <img src='/path/to/your/icon.png' alt='User Icon' className='w-6 h-6' />
                 </button>
+                <button onClick={handleLogout}>Logout</button>
             </div>
         </div>
     );

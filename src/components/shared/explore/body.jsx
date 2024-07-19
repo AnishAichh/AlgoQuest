28927cb1dd9fe37393a8f2c7efaf4264bd12412d
somir02 <somirbaruah1@gmail.com>
2024-07-19 09:43:08 +0530

    icon removed

--- src/components/shared/explore/body.jsx
+++ src/components/shared/explore/body.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from './card'
-import StarIcon from '../../../assets/star_icon.png'
-import ArrowIcon from '../../../assets/arrow_icon.png'
+// import StarIcon from '../../../assets/star_icon.png'
+// import ArrowIcon from '../../../assets/arrow_icon.png'
 import { CardData1, CardData2, CardData3 } from '../../../constants/data'
 
 const Body = () => {
@@ -10,11 +10,11 @@ const Body = () => {
             <div>
                 <div className='flex justify-between'>
                     <h2 className='font-bold text-2xl'>Featured</h2>
-                    <div className='flex justify end gap-3'>
+                    {/* <div className='flex justify end gap-3'>
                         <div><button><img src={StarIcon} alt="Star Icon" className="h-6 w-6 invert" /></button></div>
                         <div><button className='bg-indigo-800 rounded-md w-12 h-6 text-sm border-2 border-white'>More</button></div>
                         <div><button><img src={ArrowIcon} alt="Arrow Icon" className="h-6 w-6 invert" /></button></div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='container mx-auto p-6 '>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 '>
@@ -32,11 +32,11 @@ const Body = () => {
             <div>
                 <div className='flex justify-between'>
                     <h2 className='font-bold text-2xl'>Learn through some amazing courses</h2>
-                    <div className='flex justify end gap-3'>
+                    {/* <div className='flex justify end gap-3'>
                         <div><button><img src={StarIcon} alt="Star Icon" className="h-6 w-6 invert" /></button></div>
                         <div><button className='bg-indigo-800 rounded-md w-12 h-6 text-sm border-2 border-white'>More</button></div>
                         <div><button><img src={ArrowIcon} alt="Arrow Icon" className="h-6 w-6 invert" /></button></div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='container mx-auto p-6 '>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 '>
@@ -55,11 +55,11 @@ const Body = () => {
             <div>
                 <div className='flex justify-between'>
                     <h2 className='font-bold text-2xl'>Interview Sessions</h2>
-                    <div className='flex justify end gap-3'>
+                    {/* <div className='flex justify end gap-3'>
                         <div><button><img src={StarIcon} alt="Star Icon" className="h-6 w-6 invert" /></button></div>
                         <div><button className='bg-indigo-800 rounded-md w-12 h-6 text-sm border-2 border-white'>More</button></div>
                         <div><button><img src={ArrowIcon} alt="Arrow Icon" className="h-6 w-6 invert" /></button></div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='container mx-auto p-6 '>
                     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 '>
